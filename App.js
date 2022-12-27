1d81e4fea2dd457d5261cd36eb7deeac41b0f2d1
frrsfuzn <mochfarrasfauzan@gmail.com>
2022-12-27 14:51:27 +0700

    add React Query and Axios and implement fetching to pokeapi

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import axios from "axios";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+const fetchPokemon = async () => {
+  const res = await axios("https://pokeapi.co/api/v2/pokemon");
+  console.log(res);
+  return res.data;
+};
+
+const Home = () => {
+  const { data, isLoading } = useQuery(["pokemon"], fetchPokemon);
+  return (
+    <View style={styles.container}>
+      <Text>Pokedex App</Text>
+      {isLoading ? (
+        <Text>Loading...</Text>
+      ) : (
+        data.results.map((result) => (
+          <Text key={result.name}>{result.name}</Text>
+        ))
+      )}
+      <StatusBar style="auto" />
+    </View>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <QueryClientProvider client={queryClient}>
+      <Home />
+    </QueryClientProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
