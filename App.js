60a9dc86f115fbd3f3beaa55ff63120e3bb20128
frrsfuzn <mochfarrasfauzan@gmail.com>
2022-12-27 15:11:48 +0700

    add react navigator and implement it

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import axios from "axios";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 import {
   QueryClient,
   QueryClientProvider,
   useQuery,
 } from "@tanstack/react-query";
+import { NavigationContainer, StackActions } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const queryClient = new QueryClient();
+
+const Stack = createNativeStackNavigator();
 
 const fetchPokemon = async () => {
   const res = await axios("https://pokeapi.co/api/v2/pokemon");
@@ -15,7 +19,7 @@ const fetchPokemon = async () => {
   return res.data;
 };
 
-const Home = () => {
+const Home = ({ navigation }) => {
   const { data, isLoading } = useQuery(["pokemon"], fetchPokemon);
   return (
     <View style={styles.container}>
@@ -27,7 +31,20 @@ const Home = () => {
           <Text key={result.name}>{result.name}</Text>
         ))
       )}
+      <Button
+        title="Go to details"
+        onPress={() => navigation.navigate("Details")}
+      />
       <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const Details = ({navigation}) => {
+  return (
+    <View>
+      <Text>Details</Text>
+      <Button title="Go Back" onPress={() => navigation.goBack()}/>
     </View>
   );
 };
@@ -35,7 +52,12 @@ const Home = () => {
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Home />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Details" component={Details} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </QueryClientProvider>
   );
 }
