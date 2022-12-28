f14ca72949c7e94ef827733ff3c67ed456f77141
frrsfuzn <mochfarrasfauzan@gmail.com>
2022-12-28 08:44:29 +0700

    implement page your pokemon

--- navigation/Main.js
+++ navigation/Main.js
@@ -1,18 +1,26 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import Home from "../screens/Home";
-import Details from "../screens/Details";
+import PokemonLibrary from "../screens/PokemonLibrary";
+import PokemonDetails from "../screens/PokemonDetails";
 import GetPokemon from "../screens/GetPokemon";
+import YourPokemon from "../screens/YourPokemon";
 
-const HomeStack = createNativeStackNavigator();
+const YourPokemonStack = createNativeStackNavigator();
+const PokemonLibraryStack = createNativeStackNavigator();
 const GetPokemonStack = createNativeStackNavigator();
 
-const HomeStackScreens = () => (
-  <HomeStack.Navigator>
-    <HomeStack.Screen name="Home" component={Home} />
-    <HomeStack.Screen name="Details" component={Details} />
-  </HomeStack.Navigator>
+const YourPokemonStackScreens = () => (
+  <YourPokemonStack.Navigator>
+    <YourPokemonStack.Screen name="YourPokemon" component={YourPokemon} />
+  </YourPokemonStack.Navigator>
+);
+
+const PokemonLibraryStackScreens = () => (
+  <PokemonLibraryStack.Navigator>
+    <PokemonLibraryStack.Screen name="Home" component={PokemonLibrary} />
+    <PokemonLibraryStack.Screen name="Details" component={PokemonDetails} />
+  </PokemonLibraryStack.Navigator>
 );
 
 const GetPokemonStackScreens = () => (
@@ -25,7 +33,20 @@ const Tab = createBottomTabNavigator();
 
 export const Main = () => (
   <Tab.Navigator screenOptions={{ headerShown: false }}>
-    <Tab.Screen name="HomeStack" component={HomeStackScreens} options={{tabBarLabel: "Home"}} />
-    <Tab.Screen name="GetPokemonStack" component={GetPokemonStackScreens} options={{tabBarLabel: "Get Your Pokemon"}} />
+    <Tab.Screen
+      name="YourPokemonStack"
+      component={YourPokemonStackScreens}
+      options={{ tabBarLabel: "Pokemon Library" }}
+    />
+    <Tab.Screen
+      name="PokemonLibraryStack"
+      component={PokemonLibraryStackScreens}
+      options={{ tabBarLabel: "Pokemon Library" }}
+    />
+    <Tab.Screen
+      name="GetPokemonStack"
+      component={GetPokemonStackScreens}
+      options={{ tabBarLabel: "Get Your Pokemon" }}
+    />
   </Tab.Navigator>
 );
---
+++ screens/YourPokemon.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, Text} from "react-native"
+
+function YourPokemon() {
+  return (
+    <View>
+        <Text>Your Pokemon</Text>
+    </View>
+  )
+}
+
+export default YourPokemon
