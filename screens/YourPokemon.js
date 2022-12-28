289133fa4a5ead8b1fec069dcac760dccb2f3836
frrsfuzn <mochfarrasfauzan@gmail.com>
2022-12-28 09:32:21 +0700

    add webview to pokemon twitter timeline

--- screens/YourPokemon.js
+++ screens/YourPokemon.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import {View, Text} from "react-native"
+import React from "react";
+import { View, Text, SafeAreaView } from "react-native";
+import { WebView } from "react-native-webview";
 
 function YourPokemon() {
   return (
-    <View>
-        <Text>Your Pokemon</Text>
-    </View>
-  )
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ height: 200, backgroundColor: "white" }}>
+        <Text>Pokemon's Twitter</Text>
+        <WebView
+          source={{
+            html: '<a class="twitter-timeline" href="https://twitter.com/Pokemon?ref_src=twsrc%5Etfw">Tweets by Pokemon</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> ',
+          }}
+        />
+      </View>
+      <Text>Your Pokemon</Text>
+    </SafeAreaView>
+  );
 }
 
-export default YourPokemon
+export default YourPokemon;
